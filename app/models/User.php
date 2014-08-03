35c2361b97d81c3c5990a8dac9feee835865e6f1
Ricardo Tonet <ribeiro.tonet@gmail.com>
2014-08-03 17:57:04 +0100

    Add relation between User and Contact models.

--- app/models/User.php
+++ app/models/User.php
@@ -23,4 +23,9 @@ class User extends Eloquent implements UserInterface, RemindableInterface {
 	 */
 	protected $hidden = array('password', 'remember_token');
 
+	public function contacts()
+	{
+		return $this->hasMany('Contact');
+	}
+
 }
