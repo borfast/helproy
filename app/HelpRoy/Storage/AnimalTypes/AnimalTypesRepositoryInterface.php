f25fcffb22ea8e7f40e64bf2325fa43f80916be6
Ricardo Tonet <ribeiro.tonet@gmail.com>
2014-08-03 20:07:41 +0100

    Create animal type and contact repository interfaces.

---
+++ app/HelpRoy/Storage/AnimalTypes/AnimalTypesRepositoryInterface.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace HelpRoy\Storage\AnimalTypes;
+
+/**
+ * Allows access and manipulation of animal type entities.
+ */
+interface AnimalTypesRepositoryInterface 
+{	
+	/**
+	 * Returns an individual animal type based on id.
+	 * 
+	 * @param  int $id The id of the desired animal type.
+	 * @return HelpRoy\Storage\AnimalTypes\AnimalType
+	 */
+	public function find($id);
+
+	/**
+	 * Retrivies all animal types.
+	 * 
+	 * @return Illuminate\Database\Eloquent\Collection
+	 */
+	public function all();
+
+	/**
+	 * Create a new animal type.
+	 * 
+	 * @param  string $name The name of the new animal type.
+	 * @return HelpRoy\Storage\AnimalTypes\AnimalType
+	 */
+	public function create($name);
+}
---
+++ app/HelpRoy/Storage/Contacts/ContactRepositoryInterface.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace HelpRoy\Storage\Contacts;
+
+/**
+ * Allows access and manipulation of contact entities.
+ */
+interface ContactRepositoryInterface 
+{	
+	/**
+	 * Returns an individual contact based on id.
+	 * 
+	 * @param  int $id The id of the desired contact.
+	 * @return HelpRoy\Storage\Contacts\Contact
+	 */
+	public function find($id);
+
+	/**
+	 * Retrivies all contacts.
+	 * 
+	 * @return Illuminate\Database\Eloquent\Collection
+	 */
+	public function all();
+
+	/**
+	 * Create a new contact.
+	 * 
+	 * @param  array $attributes Contact entity attributes.
+	 * @return HelpRoy\Storage\Contacts\Contact
+	 */
+	public function create(array $attributes = []);
+}
