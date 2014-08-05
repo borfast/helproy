c62693eaa04e4824622cca50da62458b2daf40bf
Raúl Santos <borfast@gmail.com>
2014-08-05 16:29:51 +0100

    Rename ContactRepositoryInterface to Contacts...

--- app/HelpRoy/Storage/Contacts/ContactsRepositoryInterface.php
+++ app/HelpRoy/Storage/Contacts/ContactsRepositoryInterface.php
@@ -5,11 +5,11 @@ namespace HelpRoy\Storage\Contacts;
 /**
  * Allows access and manipulation of contact entities.
  */
-interface ContactRepositoryInterface 
-{	
+interface ContactsRepositoryInterface
+{
 	/**
 	 * Returns an individual contact based on id.
-	 * 
+	 *
 	 * @param  int $id The id of the desired contact.
 	 * @return HelpRoy\Storage\Contacts\Contact
 	 */
@@ -17,14 +17,14 @@ interface ContactRepositoryInterface
 
 	/**
 	 * Retrivies all contacts.
-	 * 
+	 *
 	 * @return Illuminate\Database\Eloquent\Collection
 	 */
 	public function all();
 
 	/**
 	 * Create a new contact.
-	 * 
+	 *
 	 * @param  array $attributes Contact entity attributes.
 	 * @return HelpRoy\Storage\Contacts\Contact
 	 */
