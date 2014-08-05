3d0431cfcee723e7dae99015ae3c9ceecc765b4d
Raúl Santos <borfast@gmail.com>
2014-08-05 17:37:27 +0100

    PSR-2 formatting

--- app/HelpRoy/Storage/AnimalTypes/AnimalTypesRepositoryInterface.php
+++ app/HelpRoy/Storage/AnimalTypes/AnimalTypesRepositoryInterface.php
@@ -6,27 +6,27 @@ namespace HelpRoy\Storage\AnimalTypes;
  * Allows access and manipulation of animal type entities.
  */
 interface AnimalTypesRepositoryInterface
-{	
-	/**
-	 * Returns an individual animal type based on id.
-	 * 
-	 * @param  int $id The id of the desired animal type.
-	 * @return HelpRoy\Storage\AnimalTypes\AnimalType
-	 */
-	public function find($id);
+{
+    /**
+     * Returns an individual animal type based on id.
+     *
+     * @param  int $id The id of the desired animal type.
+     * @return HelpRoy\Storage\AnimalTypes\AnimalType
+     */
+    public function find($id);
 
-	/**
-	 * Retrivies all animal types.
-	 * 
-	 * @return Illuminate\Database\Eloquent\Collection
-	 */
-	public function all();
+    /**
+     * Retrivies all animal types.
+     *
+     * @return Illuminate\Database\Eloquent\Collection
+     */
+    public function all();
 
-	/**
-	 * Creates a new animal type.
-	 * 
-	 * @param  string $name The name of the new animal type.
-	 * @return HelpRoy\Storage\AnimalTypes\AnimalType
-	 */
-	public function create($name);
+    /**
+     * Creates a new animal type.
+     *
+     * @param  string $name The name of the new animal type.
+     * @return HelpRoy\Storage\AnimalTypes\AnimalType
+     */
+    public function create($name);
 }
