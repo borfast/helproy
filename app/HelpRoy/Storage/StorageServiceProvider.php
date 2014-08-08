04a61f373aa8cafd8ed4b0471aff7ee9462c0415
Ricardo Tonet <ribeiro.tonet@gmail.com>
2014-08-08 18:07:50 +0100

    Update the storage service provider to include the binding of all the models.

--- app/HelpRoy/Storage/StorageServiceProvider.php
+++ app/HelpRoy/Storage/StorageServiceProvider.php
@@ -6,13 +6,49 @@ use Illuminate\Support\ServiceProvider;
 
 class StorageServiceProvider extends ServiceProvider
 {
+    // Triggered automatically by Laravel
+    public function register()
+    {
+        // Contacts
+        $this->app->bind(
+          'HelpRoy\Storage\Contacts\ContactsRepositoryInterface',
+          'HelpRoy\Storage\Contacts\EloquentContactsRepository'
+        );
 
-   // Triggered automatically by Laravel
-   public function register()
-   {
-       $this->app->bind(
-           'HelpRoy\Storage\Contacts\ContactsRepositoryInterface',
-           'HelpRoy\Storage\Contacts\EloquentContactsRepository'
-       );
-   }
+        // Users
+        $this->app->bind(
+          'HelpRoy\Storage\Users\UsersRepositoryInterface',
+          'HelpRoy\Storage\Users\EloquentUsersRepository'
+        );
+
+        // Animals
+        $this->app->bind(
+          'HelpRoy\Storage\Animals\AnimalsRepositoryInterface',
+          'HelpRoy\Storage\Animals\EloquentAnimalsRepository'
+        );
+
+        // AnimalTypes
+        $this->app->bind(
+          'HelpRoy\Storage\AnimalTypes\AnimalTypesRepositoryInterface',
+          'HelpRoy\Storage\AnimalTypes\EloquentAnimalTypesRepository'
+        );
+
+        // AdoptionAds
+        $this->app->bind(
+          'HelpRoy\Storage\Ads\AdoptionAdsRepositoryInterface',
+          'HelpRoy\Storage\Ads\EloquentAdoptionAdsRepository'
+        );
+
+        // LostAds
+        $this->app->bind(
+          'HelpRoy\Storage\Ads\LostAdsRepositoryInterface',
+          'HelpRoy\Storage\Ads\EloquentLostAdsRepository'
+        );
+
+        // FoundAds
+        $this->app->bind(
+          'HelpRoy\Storage\Ads\FoundAdsRepositoryInterface',
+          'HelpRoy\Storage\Ads\EloquentFoundAdsRepository'
+        );
+    }
 }
