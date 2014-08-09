c0cb422726e7903f7e236bfb7ca15c6a9cbce319
Ricardo Tonet <ribeiro.tonet@gmail.com>
2014-08-09 18:03:58 +0100

    PSR-2 formatting.

--- app/routes.php
+++ app/routes.php
@@ -11,42 +11,41 @@
 |
 */
 
-Route::get('/', function()
-{
-	return View::make('hello');
+Route::get('/', function () {
+    return View::make('hello');
 });
 
-Route::group(['prefix' => 'contacts'], function() {
-	Route::model('contact', 'HelpRoy\Storage\Contacts\Contact');
-	Route::get('show/{contact}', ['uses' => 'ContactsController@show']);
+Route::group(['prefix' => 'contacts'], function () {
+    Route::model('contact', 'HelpRoy\Storage\Contacts\Contact');
+    Route::get('show/{contact}', ['uses' => 'ContactsController@show']);
 });
 
-Route::group(['prefix' => 'animals'], function() {
-	Route::model('animal', 'HelpRoy\Storage\Animals\Animal');
-	Route::get('show/{animal}', ['uses' => 'AnimalsController@show']);
+Route::group(['prefix' => 'animals'], function () {
+    Route::model('animal', 'HelpRoy\Storage\Animals\Animal');
+    Route::get('show/{animal}', ['uses' => 'AnimalsController@show']);
 });
 
-Route::group(['prefix' => 'animaltypes'], function() {
-	Route::model('animaltypes', 'HelpRoy\Storage\AnimalTypes\AnimalType');
-	Route::get('show/{animaltypes}', ['uses' => 'AnimalTypesController@show']);
+Route::group(['prefix' => 'animaltypes'], function () {
+    Route::model('animaltypes', 'HelpRoy\Storage\AnimalTypes\AnimalType');
+    Route::get('show/{animaltypes}', ['uses' => 'AnimalTypesController@show']);
 });
 
-Route::group(['prefix' => 'users'], function() {
-	Route::model('user', 'HelpRoy\Storage\Users\User');
-	Route::get('show/{user}', ['uses' => 'UsersController@show']);
+Route::group(['prefix' => 'users'], function () {
+    Route::model('user', 'HelpRoy\Storage\Users\User');
+    Route::get('show/{user}', ['uses' => 'UsersController@show']);
 });
 
-Route::group(['prefix' => 'lostads'], function() {
-	Route::model('lostad', 'HelpRoy\Storage\Ads\LostAd');
-	Route::get('show/{lostad}', ['uses' => 'LostAdsController@show']);
+Route::group(['prefix' => 'lostads'], function () {
+    Route::model('lostad', 'HelpRoy\Storage\Ads\LostAd');
+    Route::get('show/{lostad}', ['uses' => 'LostAdsController@show']);
 });
 
-Route::group(['prefix' => 'foundads'], function() {
-	Route::model('foundad', 'HelpRoy\Storage\Ads\FoundAd');
-	Route::get('show/{foundad}', ['uses' => 'FoundAdsController@show']);
+Route::group(['prefix' => 'foundads'], function () {
+    Route::model('foundad', 'HelpRoy\Storage\Ads\FoundAd');
+    Route::get('show/{foundad}', ['uses' => 'FoundAdsController@show']);
 });
 
-Route::group(['prefix' => 'adoptionads'], function() {
-	Route::model('adoptionad', 'HelpRoy\Storage\Ads\AdoptionAd');
-	Route::get('show/{adoptionad}', ['uses' => 'AdoptionAdsController@show']);
+Route::group(['prefix' => 'adoptionads'], function () {
+    Route::model('adoptionad', 'HelpRoy\Storage\Ads\AdoptionAd');
+    Route::get('show/{adoptionad}', ['uses' => 'AdoptionAdsController@show']);
 });
