1d280eac09b7bcf2ca6f49a65fcf325c5717bcad
Ricardo Tonet <ribeiro.tonet@gmail.com>
2014-08-09 17:54:16 +0100

    Change production database configuration to default data.

--- app/config/database.php
+++ app/config/database.php
@@ -55,9 +55,9 @@ return array(
 		'mysql' => array(
 			'driver'    => 'mysql',
 			'host'      => 'localhost',
-			'database'  => 'helproy',
-			'username'  => 'rtonet',
-			'password'  => 'pass',
+			'database'  => 'database',
+			'username'  => 'username',
+			'password'  => 'password',
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
