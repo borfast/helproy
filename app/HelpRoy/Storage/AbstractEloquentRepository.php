faa814208d3ef74c5de706d13b14f66aa51b9671
Raúl Santos <borfast@gmail.com>
2014-11-08 17:23:17 +0000

    Add create() method to AbstractEloquentRepository

--- app/HelpRoy/Storage/AbstractEloquentRepository.php
+++ app/HelpRoy/Storage/AbstractEloquentRepository.php
@@ -24,11 +24,26 @@ abstract class AbstractEloquentRepository
 
     /**
      * Return the specified model instance
+     *
      * @param  integer $id  The ID of the model you want to find.
      * @return mixed|null  The specified model instance or null if it doesn't exist.
      */
     public function find($id)
     {
         return $this->model->find($id);
+    }
+
+
+    /**
+     * Creates a new instance of the model.
+     *
+     * @param  array $attributes Model entity attributes.
+     * @return Illuminate\Database\Eloquent\Model
+     */
+    public function create(array $attributes = [])
+    {
+        $instance = $this->model->create($attributes);
+
+        return $instance;
     }
 }
